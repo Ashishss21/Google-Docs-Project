c1e156d50f83802ff0dbe0d2476d7abdd2df50cb
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2022-02-01 17:45:16 +0530

    Delete firebase.config.js

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAj4JrUnDK9Eat1Z2uVpg6h25chCQxxJKA",
-  authDomain: "docs-94968.firebaseapp.com",
-  projectId: "docs-94968",
-  storageBucket: "docs-94968.appspot.com",
-  messagingSenderId: "774695656953",
-  appId: "1:774695656953:web:a82a7216cac0184b947128",
-};
